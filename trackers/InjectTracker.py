1b4772926f74f7439c64deef937e970ff8ebe257
Blazej Czapp <bczapp@constant.space>
2020-01-26 16:26:13 +0000

    Correctly clamp inject/idle intervals to game length and hatchery lifetime

--- trackers/InjectTracker.py
+++ trackers/InjectTracker.py
@@ -3,7 +3,7 @@ import matplotlib.patches as mpatches
 from .Tracker import Tracker
 from matplotlib.ticker import FuncFormatter
 from replay_helpers import Entity, timestamp, real_seconds
-from sc2reader.events.tracker import UnitBornEvent, UnitDoneEvent
+from sc2reader.events.tracker import UnitBornEvent, UnitDoneEvent, UnitDiedEvent
 from sc2reader.events.game import TargetUnitCommandEvent, PlayerLeaveEvent
 
 class InjectTracker(Tracker):
@@ -11,8 +11,7 @@ class InjectTracker(Tracker):
 
     def __init__(self, player_name):
         Tracker.__init__(self, player_name)
-        self.hatchery_ids = [] # to store order and time of creation
-        self.hatchery_injects = {} # list of tuples of (inject_start, inject_end)
+        self.hatchery_history = {}
 
     def consume_event(self, event):
         # the initial hatchery is "born", subsequent ones are "done"
@@ -21,21 +20,32 @@ class InjectTracker(Tracker):
            and event.unit.owner is not None \
            and event.unit.owner.name.startswith(self.player_name) \
            and (event.unit.name == "Hatchery" or event.unit.name == "Lair" or event.unit.name == "Hive"):
-            self.hatchery_ids.append((event.unit_id, event.second))
-            self.hatchery_injects[event.unit_id] = []
+            self.hatchery_history[event.unit_id] = {'injects': [], 'created': event.second, 'destroyed': None}
 
         elif isinstance(event, UnitDoneEvent) \
              and event.unit.owner is not None \
              and event.unit.owner.name.startswith(self.player_name) \
              and (event.unit.name == "Hatchery" or event.unit.name == "Lair" or event.unit.name == "Hive"):
-            self.hatchery_ids.append((event.unit_id, event.second))
-            self.hatchery_injects[event.unit_id] = []
+            self.hatchery_history[event.unit_id] = {'injects': [], 'created': event.second, 'destroyed': None}
+
+        elif isinstance(event, UnitDiedEvent) \
+             and event.unit.owner is not None \
+             and event.unit.owner.name.startswith(self.player_name) \
+             and (event.unit.name == "Hatchery" or event.unit.name == "Lair" or event.unit.name == "Hive"):
+            try:
+                self.hatchery_history[event.unit_id]['destoryed'] = event.second
+            except:
+                # if the hatchery never finished, we don't record its death
+                pass
 
         elif type(event) == TargetUnitCommandEvent \
+             and event.player.name is not None \
+             and event.player.name.startswith(self.player_name) \
              and hasattr(event, "ability") \
              and event.ability_name == "SpawnLarva":
             # TODO using isinstance() gives false positives on some Right Click events - fix in sc2reader?
-            inject_intervals = self.hatchery_injects[event.target_unit_id]
+            inject_intervals = self.hatchery_history[event.target_unit_id]['injects']
+
             if inject_intervals and event.second < inject_intervals[-1][1]:
                 # queueing inject, starts after the latest one finishes (may be queued itself)
                 current_inject_end = inject_intervals[-1][1]
@@ -56,20 +66,28 @@ class InjectTracker(Tracker):
         axes.xaxis.set_major_formatter(FuncFormatter(x_to_timestamp))
 
         h = 1
-        for (hatch, creation) in self.hatchery_ids:
-            injects = self.hatchery_injects[hatch]
+        # plot hatcheries sorted by creation time
+        sorted_hatcheries = sorted(self.hatchery_history.items(), key=lambda item: item[1]['created'])
+        for hatch in [item[1] for item in sorted_hatcheries]:
+            injects = hatch['injects']
+            creation = hatch['created']
+            life_end = self.game_end if hatch['destroyed'] is None else hatch['destroyed']
+
+            # clamp all intervals to game duration or hatchery life time
+            injects = [(min(life_end, interval[0]), min(life_end, interval[1])) for interval in injects]
 
             # all injected intervals
-            axes.broken_barh([(interval[0], interval[1]-interval[0]) for interval in injects], (5*h, 2), facecolors='tab:green', alpha=0.9)
+            for interval in injects:
+                axes.broken_barh([(interval[0], interval[1]-interval[0]) for interval in injects], (5*h, 2), facecolors='tab:green', alpha=0.9)
 
             if injects:
                 # idle time from creation until first inject
                 axes.broken_barh([(creation, injects[0][0] - creation)], (5*h, 2), facecolors='tab:red', alpha=0.75)
-                # idle time from last inject until game end
-                axes.broken_barh([(injects[-1][1], self.game_end-injects[-1][1])], (5*h, 2), facecolors='tab:red', alpha=0.75)
+                # idle time from last inject until game end or death
+                axes.broken_barh([(injects[-1][1], life_end-injects[-1][1])], (5*h, 2), facecolors='tab:red', alpha=0.75)
             else:
-                # never injected - plot idle time from creation until game end
-                axes.broken_barh([(creation, self.game_end-creation)], (5*h, 2), facecolors='tab:red', alpha=0.75)
+                # never injected - plot idle time from creation until destruction or game end
+                axes.broken_barh([(creation, life_end-creation)], (5*h, 2), facecolors='tab:red', alpha=0.75)
 
             if len(injects) > 1:
                 # idle time between injects
@@ -80,6 +98,6 @@ class InjectTracker(Tracker):
         injected_legend = mpatches.Patch(color='tab:green', label='injected', alpha=0.9)
 
         axes.set_yticks([5*i+1 for i in range(1, h+1)])
-        axes.set_yticklabels(['Hatch %d' % i for i in range(1, len(self.hatchery_ids)+1)])
+        axes.set_yticklabels(['Hatch %d' % i for i in range(1, len(self.hatchery_history)+1)])
         
         axes.legend(handles=[injected_legend, idle_legend], loc='upper left')
